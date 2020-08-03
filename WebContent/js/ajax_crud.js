ff462e59c45f567b533b3f9f00868db53587c754
chenglin0413 <chenglin0413@gmail.com>
2020-08-03 18:09:24 +0800

    abstractiong function , especially ajax_crud.js

--- WebContent/js/ajax_crud.js
+++ WebContent/js/ajax_crud.js
@@ -1,23 +1,16 @@
- 	function ajax_post(map){//insert to restdb
+	function isNull(str){
+        if(str == null || str == undefined || str == "") return true;
+    }
+	function ajax_post(data_before_transfer_by_ajax){//insert to restdb
     	//console.log("name :"+map["name"]+" tel :"+map["tel"]+" type_index :"+map["type_index"]+"timestamp: "+map["timestamp"]);
-    	let data = {action:"insert",
-        	        data:{name: map["name"].trim()
-                        ,tel: map["tel"]
-                        ,notes: map["notes"]
-                        ,type: map["type"]
-                        ,type_index: map["type_index"]
-                        ,gender: map["gender"]
-                        ,timestamp: new Date(map["timestamp"]).toUTCString()
-                     }
-                    };
-    	console.log(map["name"]);
-        if(!isNull(map["name"].trim())){
+    	let data_need__transfer = data_before_transfer_by_ajax;
+        if(!isNull(data_need__transfer["data"]["name"])){
         	$.ajax({
     		    type: "POST",
     		    url: "AdrbookServlet",
-    		    async: true,
+    		    async: false,
     		    contentType: "application/json", // NOT dataType!
-    		    data: JSON.stringify(data),
+    		    data: JSON.stringify(data_need__transfer),
     		    success: function(response) {
     	            console.log(response);
     		    }
@@ -35,12 +28,12 @@
     	$.ajax({
 		    type: "POST",
 		    url: "AdrbookServlet",
-		    async: true,
+		    async: false,
 		    contentType: "application/json", // NOT dataType!
 		    data: JSON.stringify(data),
 		    success: function(response) {
-	            result = JSON.parse(response);//json自串 轉成 json物件
-	            //console.log(result);
+	            result = JSON.parse(response);//json Str trasfer json Obj
+	            console.log("result:"+result);
 		    }
 		});
         return result;
@@ -60,7 +53,7 @@
 		$.ajax({
 		    type: "POST",
 		    url: "AdrbookServlet",
-		    async: true,
+		    async: false,
 		    contentType: "application/json", // NOT dataType!
 		    data: JSON.stringify(data),
 		    success: function(response) {
@@ -76,11 +69,31 @@
 	 	$.ajax({
 			    type: "POST",
 			    url: "AdrbookServlet",
-			    async: true,
+			    async: false,
 			    contentType: "application/json", // NOT dataType!
 			    data: JSON.stringify(data),
 			    success: function(response) {
 		            console.log(response);
 			    }
 			});
+    }
+
+    function data_before_transfer_by_ajax(one_adrbook_data,crud_str){
+    	let data ='';
+    	if(crud_str=="insert"){
+    		data = {action:crud_str,
+        	        data:{name: one_adrbook_data["name"].trim()
+                        ,tel: one_adrbook_data["tel"]
+                        ,notes: one_adrbook_data["notes"]
+                        ,type: one_adrbook_data["type"]
+                        ,type_index: one_adrbook_data["type_index"]
+                        ,gender: one_adrbook_data["gender"]
+                     }
+                    };
+    	}else if (crud_str=="getAll"){
+    		data = {
+        		    action: crud_str
+        		};
+    	}
+       return data;
     }
